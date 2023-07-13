bb38576be3ab4631ae675f1c2c1503fa3051893c
HumaCode <HumaCode>
2023-07-13 20:31:06 +0700

    Finally 2

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,81 +1,101 @@
 @extends('backend.layouts.app')
 
 @php
-$merek = App\Models\BrandMobil::count();
-$mobil = App\Models\Mobil::count();
+    $merek = App\Models\BrandMobil::count();
+    $mobil = App\Models\Mobil::count();
+    $merek_motor = App\Models\MerekMotor::count();
+    $motor = App\Models\Motor::count();
 @endphp
 
 @section('heading')
-<div class="row page-titles mx-0">
-    <div class="col-sm-6 p-md-0">
-        <div class="welcome-text">
-            <h4>Halo, Selamat Datang Kembali!</h4>
-            <p class="mb-0">{{ auth()->user()->name }}</p>
+    <div class="row page-titles mx-0">
+        <div class="col-sm-6 p-md-0">
+            <div class="welcome-text">
+                <h4>Halo, Selamat Datang Kembali!</h4>
+                <p class="mb-0">{{ auth()->user()->name }}</p>
+            </div>
+        </div>
+
+        <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
         </div>
     </div>
-
-    <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-    </div>
-</div>
 @endsection
 
 @section('content')
-
-<div class="row">
-    <div class="col-lg-6 col-sm-6">
-        <div class="card">
-            <div class="stat-widget-one card-body">
-                <div class="stat-icon d-inline-block">
-                    <i class="ti-layout-grid2 text-pink border-pink"></i>
+    <div class="row">
+        <div class="col-lg-6 col-sm-6">
+            <a href="{{ route('merek') }}">
+                <div class="card">
+                    <div class="stat-widget-one card-body">
+                        <div class="stat-icon d-inline-block">
+                            <i class="ti-layout-grid2 text-pink border-pink"></i>
+                        </div>
+                        <div class="stat-content d-inline-block">
+                            <div class="stat-text">Merek Mobil</div>
+                            <div class="stat-digit">{{ $merek }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="stat-content d-inline-block">
-                    <div class="stat-text">Merek Mobil</div>
-                    <div class="stat-digit">{{ $merek }}</div>
+            </a>
+        </div>
+        <div class="col-lg-6 col-sm-6">
+
+            <a href="{{ route('mobil') }}">
+                <div class="card">
+                    <div class="stat-widget-one card-body">
+                        <div class="stat-icon d-inline-block">
+                            <i class="ti-car text-success border-success"></i>
+                        </div>
+                        <div class="stat-content d-inline-block">
+                            <div class="stat-text">Daftar Mobil</div>
+                            <div class="stat-digit">{{ $mobil }}</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6 col-sm-6">
-        <div class="card">
+            </a>
 
-            <div class="stat-widget-one card-body">
-                <div class="stat-icon d-inline-block">
-                    <i class="ti-car text-success border-success"></i>
+        </div>
+
+        <div class="col-lg-6 col-sm-6">
+
+            <a href="{{ route('merek_motor') }}">
+                <div class="card">
+                    <div class="stat-widget-one card-body">
+                        <div class="stat-icon d-inline-block">
+                            <i class="ti-layout-grid2 text-pink border-pink"></i>
+                        </div>
+                        <div class="stat-content d-inline-block">
+                            <div class="stat-text">Merek Motor</div>
+                            <div class="stat-digit">{{ $merek_motor }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="stat-content d-inline-block">
-                    <div class="stat-text">Daftar Mobil</div>
-                    <div class="stat-digit">{{ $mobil }}</div>
+            </a>
+
+        </div>
+        <div class="col-lg-6 col-sm-6">
+            <a href="{{ route('motor') }}">
+                <div class="card">
+
+                    <div class="stat-widget-one card-body">
+                        <div class="stat-icon d-inline-block">
+                            <i class="fa fa-motorcycle text-success border-success"></i>
+                            {{-- <i class="ti-car "></i> --}}
+                        </div>
+                        <div class="stat-content d-inline-block">
+                            <div class="stat-text">Daftar Motor</div>
+                            <div class="stat-digit">{{ $motor }}</div>
+                        </div>
+                    </div>
+
+
                 </div>
-            </div>
-
+            </a>
 
         </div>
+
     </div>
-
-</div>
-
-
-{{-- <div class="row">
-    <div class="col-lg-8">
-        <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">Fee Collections and Expenses</h4>
-            </div>
-            <div class="card-body">
-                <div class="ct-bar-chart mt-5"></div>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="card">
-            <div class="card-body">
-                <div class="ct-pie-chart"></div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-
 @endsection
